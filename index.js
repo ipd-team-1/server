a392f1ae0220a68afc411472c5a42dc47f515779
David Zhu <hdavidzhu@gmail.com>
2016-11-12 09:36:47 -0500

    ADD: Express boilerplate.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express      = require('express');
+const morgan       = require('morgan');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer       = require('multer');
+
+const app = express();
+
+// Middleware.
+app.use(morgan('combined'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+// TODO: Use multer.
+
+app.get('/', function(req, res) {
+  res.send("GET request to homepage");
+});
+
+app.listen(3000);
